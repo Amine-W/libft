6faa5a7e33e8adfe878f2408d632d99bbd7f3af0
Amine-W <aminewahab37@gmail.com>
2025-04-06 10:08:50 +0200

    isdigit

---
+++ ft_isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int ft_isdigit(int c)
+{
+    if (c >= '0'&& c <= '9')
+        return (1);
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    printf("%i\n",ft_isdigit('1'));
+}
