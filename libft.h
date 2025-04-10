c6a32b7ceb92bd28c870c96cdb8ff15a98334723
Amine-W <aminewahab37@gmail.com>
2025-04-10 08:13:00 +0200

    ft_putnbr_fd

--- libft.h
+++ libft.h
@@ -31,5 +31,9 @@ size_t ft_strlen(const char *str);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 int ft_tolower(int c);
 int ft_toupper(int c);
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
